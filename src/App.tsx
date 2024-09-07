38cf6734042d4c791900f2bd80dffc4eba1c5a9e
Daniel Albanez <danielalbanez40@gmail.com>
2024-09-07 19:25:33 -0300

    Maquetando menú - primeros pasos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
-      Hello 
+      <Header />
+      
+     <h1 className="text-3xl font-bold bg-red-400">
+      Hello from branch daniel/landing-page-header
     </h1>
     </>
   )
