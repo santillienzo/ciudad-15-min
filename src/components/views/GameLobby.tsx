ef282a9d1c5607f114667a38bf0b5910a292db46
santillienzo <enzo.135246@gmail.com>
2024-09-10 21:06:16 -0300

    feat: integrate shadui

--- src/components/views/GameLobby.tsx
+++ src/components/views/GameLobby.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button"
+
 const GameLobby = () => {
   return (
-    <div className="">
-      
+    <div className="flex items-end w-full h-full bg-red-50">
+      <div className="flex flex-col">
+        <Button>Jugar</Button>
+        <Button variant='outline'>Registrate</Button>
+      </div>
     </div>
   )
 }
