68050899cd7907dea5cd53fa714d2478131949f9
enzosantilli <enzo.santilli@latinad.com>
2024-09-16 19:48:53 -0300

    chore: Chakra only AppLanding

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { ChakraProvider } from "@chakra-ui/react";
-import AppLanding from "./components/views/landingPage/AppLanding.tsx";
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'sonner'
