b8e94288dba8a8815d154fd85f69857d6518ce4d
Daniel Albanez <danielalbanez40@gmail.com>
2024-10-09 09:42:49 -0300

    about us section finished

--- src/components/features/landingPage/Sponsors.tsx
+++ src/components/features/landingPage/Sponsors.tsx
@@ -1,88 +1,43 @@
 import { Box, Stack, Text } from "@chakra-ui/react";
 // import Logo from "../../../assets/Logo.svg";
 import { Image } from "@chakra-ui/react";
-
-// const LOGOS = [
-//   <img src={Logo} alt="Logo" />,
-//   <img src={Logo} alt="Logo" />,
-//   <img src={Logo} alt="Logo" />,
-//   <img src={Logo} alt="Logo" />,
-//   <img src={Logo} alt="Logo" />,
-//   <img src={Logo} alt="Logo" />,
-//   <img src={Logo} alt="Logo" />,
-//   <img src={Logo} alt="Logo" />,
-//   <img src={Logo} alt="Logo" />,
-//   <img src={Logo} alt="Logo" />,
-//   <img src={Logo} alt="Logo" />,
-//   <img src={Logo} alt="Logo" />,
-//   <img src={Logo} alt="Logo" />,
-//   <img src={Logo} alt="Logo" />,
-// ];
+import Logo from "@/assets/svg/Logo blanco.svg";
+import Edificios_1 from "@/assets/svg/Edificios 1.svg";
 
 const Sponsors = () => {
   return (
     <Stack
       id="sponsors"
-      className="scroll-smooth bg-background-secondary-muted flex justify-center items-center h-screen w-full overflow-hidden"
+      className="relative scroll-smooth bg-[#4AAFB2] flex justify-center items-center h-screen w-full overflow-hidden"
     >
-          <Text className="flex flex-col text-5xl p-8 font-bold text-background-secondary-foreground">
-            Sponsors
+      <Box className="flex w-[90%] justify-end items-center h-full relative">
+        <Box className="flex flex-col w-1/2 max-sm:w-full gap-8 z-10">
+          <Image src={Logo} alt="Edificio" className="w-96" />
+
+          <Text className="font-bold text-button-cta-primary-foreground">
+            La{" "}
+            <span className="text-button-cta-primary"> ciudad de Mendoza </span>
+            enfrenta importantes desafíos relacionados con la movilidad y el
+            acceso equitativo a los servicios esenciales.
           </Text>
-      <Box className="flex w-screen justify-evenly max-sm:flex-col max-sm:items-center max-sm:h-screen max-sm:max-w-sm">
-        <Box className="">
-          <Image
-            src="https://img.freepik.com/vector-gratis/coleccion-etiquetas-patrocinador_23-2148725863.jpg"
-            alt="Sponsors"
-            className="h-[70%] max-sm:h-[12rem]"
-          />
-        </Box>
-        <Box>
-          <Image
-            src="https://img.freepik.com/vector-gratis/coleccion-etiquetas-patrocinador_23-2148725863.jpg"
-            alt="Sponsors"
-            className="h-[70%] max-sm:h-[12rem]"
-          />
-        </Box>
-        <Box>
-          <Image
-            src="https://img.freepik.com/vector-gratis/coleccion-etiquetas-patrocinador_23-2148725863.jpg"
-            alt="Sponsors"
-            className="h-[70%] max-sm:h-[12rem]"
-          />
+
+          <Text className="font-bold text-button-cta-primary-foreground">
+            Esta campaña busca sensibilizar a la población sobre la{" "}
+            <span className="text-button-cta-primary"> movilidad activa </span>{" "}
+            y la{" "}
+            <span className="text-button-cta-primary">
+              {" "}
+              reducción del uso del automóvil
+            </span>
+            , promoviendo una cultura de caminatas y ciclismo que favorezca
+            tanto al medio ambiente como a la salud pública.
+          </Text>
         </Box>
       </Box>
-      {/* <Box className="flex flex-col overflow-hidden"> */}
-      {/* <Box className="flex flex-col items-center justify-center h-[30vh] mb-52">
-          <Text className="text-5xl text-center font-bold flex flex-col text-[#07C15B]">
-            {" "}
-            <span className="font-semibold text-3xl leading-[5rem]">
-              Te presentamos a{" "}
-            </span>{" "}
-            NUESTROS SPONSORS
-          </Text>
-        </Box> */}
+        <Box className="w-1/2 max-sm:hidden">
+          <Image src={Edificios_1} alt="Edificio" className="absolute bottom-0 left-0 w-96 m-0 p-0" />
 
-      {/* <Box className="relative m-auto w-screen flex items-cente overflow-hidden bg-transparent before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,rgba(255,255,255,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,rgba(255,255,255,0)_100%)] after:content-['']">
-          <Box className="animate-infinite-slider flex w-[calc(250px*10)">
-            {LOGOS.map((logo, index) => (
-              <Box
-                className="slide flex w-[125px] items-center justify-center"
-                key={index}
-              >
-                {logo}
-              </Box>
-            ))}
-            {LOGOS.map((logo, index) => (
-              <Box
-                className="slide flex w-[125px] items-center justify-center"
-                key={index}
-              >
-                {logo}
-              </Box>
-            ))}
-          </Box>
         </Box>
-      </Box> */}
     </Stack>
   );
 };
