91e6ca36fbda5c763c24c3e15453bbd5186754b3
santillienzo <enzo.135246@gmail.com>
2024-10-10 14:29:11 -0300

    style: some issues

--- src/components/layout/landingPage/Header.tsx
+++ src/components/layout/landingPage/Header.tsx
@@ -1,4 +1,4 @@
-import { RiMenuFill, RiCloseCircleLine } from "react-icons/ri";
+import { RiMenuFill, RiCloseLine } from "react-icons/ri";
 import { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 
@@ -49,10 +49,10 @@ const Header = () => {
   ];
 
   return (
-    <header className="fixed max-sm:bg-transparent bg-background-primary flex items-center justify-between xl:justify-start w-full py-4 px-6 md:px-8 h-16 md:h-20 z-50 top-0 left-0">
+    <header className="fixed max-sm:bg-transparent flex items-center justify-between xl:justify-start w-full py-4 px-6 md:px-8 h-16 md:h-20 z-50 top-0 left-0">
       <nav
         ref={menuMobileRef}
-        className={`bg-background-primary fixed w-[80%] md:w-[60%] xl:w-full h-full top-0 ${
+        className={`bg-background-primary fixed w-[80%] md:w-[60%] xl:w-full shadow-xl h-full top-0 ${
           menuMobile ? "left-0" : "-left-full"
         } xl:static xl:h-auto flex-1 justify-center flex flex-col xl:flex-row items-center gap-10 transition-all duration-500 ease-in-out z-40`}
       >
@@ -87,7 +87,7 @@ const Header = () => {
             variants={iconVariants}
             transition={{ duration: 0.3 }}
           >
-            <RiCloseCircleLine className="text-button-cta-secondary-foreground" />
+            <RiCloseLine className="text-button-cta-secondary-foreground" />
           </motion.div>
         ) : (
           <motion.div
