6b5e3d878e587bd8eca42fdc3c8abc56e56cc6e4
santillienzo <enzo.135246@gmail.com>
2024-10-24 23:27:54 -0300

    some changes

--- src/components/features/qrGenerator/QrCodeGenerator.tsx
+++ src/components/features/qrGenerator/QrCodeGenerator.tsx
@@ -1,20 +1,21 @@
 import { QRCodeSVG } from "qrcode.react";
 import React, { useEffect, useRef, useState } from "react";
-import qrInfo from "./qr-info.json";
+import qrInfo from "@/lib/data/locations.json";
 import { useReactToPrint } from "react-to-print";
 import { Button } from "@chakra-ui/react";
 // import QRCode from "react-qr-code";
 
 interface QRData {
-  id: number;
-  location_id: number;
+  id: string;
   source: string;
   name: string;
-  type: string;
-  subtype: string;
-  direction: string;
-  lat: number;
-  long: number;
+  category: string;
+  subcategory: string;
+  direction?: string;
+  coord: {
+    lat: number;
+    long: number;
+  }
 }
 
 const QrCodeGenerator: React.FC = () => {
@@ -26,7 +27,12 @@ const QrCodeGenerator: React.FC = () => {
   
 
   useEffect(() => {
-    setQrCodes(qrInfo.slice(0, 50)); // Carga los primeros 50 códigos QR
+    setQrCodes(qrInfo.locations.map(location =>{
+      return {
+        ...location,
+        source: 'ciudad-15-minutos'
+      }
+    })); // Carga los primeros 50 códigos QR
   }, []);
 
   return (
@@ -35,7 +41,7 @@ const QrCodeGenerator: React.FC = () => {
         <div className="w-[90%] px-[2rem]">
           <h2 className="text-3xl text-center">QrCodeGen App</h2>
           <Button
-            onClick={reactToPrintFn}
+            onClick={()=> reactToPrintFn()}
             style={{
               textAlign: "center",
               textDecoration: "none",
@@ -69,10 +75,17 @@ const QrCodeGenerator: React.FC = () => {
                   }}
                 >
                   <h3>{`${item.name}`}</h3>
-                  <div id={`qr-code-${item.location_id}`}>
+                  <div id={`qr-code-${item.id}`}>
                     <QRCodeSVG
-                      value={`${item.id}, ${item.location_id}, ${item.source}`}
-                      level="H"
+                      value={`{
+                          "id": "${item.id}",
+                          "source": "${item.source}",
+                          "name": "${item.name}",
+                          "category": "${item.category}",
+                          "subcategory": "${item.subcategory}",
+                        }`
+                      }
+                      level="L"
                       style={{
                         width: "100%" /* Ajustar a 100% del contenedor */,
                         height: "auto",
